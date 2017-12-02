e84719f7e8ece01996413f3436c9b7f71aa8dc4b
michiganzmann <michiganzmann@users.noreply.github.com>
2017-12-02 01:54:51 +0100

    EventHandler V1

--- src/main/java/ch/ethz/matsim/courses/abmt17_template/analysis/RunWithEventHandler.java
+++ src/main/java/ch/ethz/matsim/courses/abmt17_template/analysis/RunWithEventHandler.java
@@ -1,5 +1,12 @@
 package ch.ethz.matsim.courses.abmt17_template.analysis;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.core.api.experimental.events.EventsManager;
@@ -11,8 +18,15 @@ import org.matsim.core.events.MatsimEventsReader;
 import org.matsim.core.network.io.NetworkReaderMatsimV2;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.IOUtils;
 
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+import ch.ethz.matsim.av.electric.assets.station.Station;
+import ch.ethz.matsim.av.electric.tracker.ConsumptionTracker;
 import ch.ethz.matsim.av.framework.AVConfigGroup;
+import ch.ethz.matsim.courses.abmt17_template.StationReader;
 
 public class RunWithEventHandler {
 
@@ -21,8 +35,10 @@ public class RunWithEventHandler {
 		String configPath = "scenario/abmt_config.xml";
 		String populationPath = "scenario/abmt_population.xml.gz";
 		String networkPath = "scenario/abmt_network.xml.gz";
+		
 		String eventsPath = "scenario/80.events.xml.gz";
-
+		
+		
 		Config config = ConfigUtils.createConfig();
 		Scenario scenario = ScenarioUtils.createScenario(config);
 		
@@ -34,13 +50,65 @@ public class RunWithEventHandler {
 		
 		networkReader.readFile(networkPath);
 		
+		
+		
 		EventsManager eventsManager = EventsUtils.createEventsManager();
 		
-		eventsManager.addHandler(new StationEventHandler());
 		
 		MatsimEventsReader eventsReader = new MatsimEventsReader(eventsManager);
 		
+
+		ConsumptionTracker tracker = null;
+		StationReader reader = new StationReader(scenario.getNetwork(), tracker, eventsManager);
+		Collection<Station> stations = null;
+		try {
+			stations = reader.read(new File("scenario/stations_zuerich.csv"));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		StationEventHandler stationEventHandler = new StationEventHandler(stations);
+		eventsManager.addHandler(stationEventHandler);
+		
 		eventsReader.readFile(eventsPath);
+		
+		FileWriter fileWriter = null;
+		BufferedWriter writer = null;
+		try {
+			fileWriter = new FileWriter("scenario/StationChargings.csv");
+			writer = new BufferedWriter(fileWriter);
+			writer.write("StationId,numberOfArrivals");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		Map<Station,Integer> stationArrivals = stationEventHandler.getStartRecharge();
+		for (Map.Entry<Station, Integer> entry : stationArrivals.entrySet()) {
+			System.out.println(entry.getKey().getId() +","+ entry.getValue());
+			try {
+				writer.newLine();
+				writer.write(entry.getKey().getId() +","+ entry.getValue());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		try {
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
 	}
--- src/main/java/ch/ethz/matsim/courses/abmt17_template/analysis/StationEventHandler.java
+++ src/main/java/ch/ethz/matsim/courses/abmt17_template/analysis/StationEventHandler.java
@@ -1,12 +1,27 @@
 package ch.ethz.matsim.courses.abmt17_template.analysis;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.GenericEvent;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.handler.GenericEventHandler;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
 import org.matsim.contrib.dvrp.data.Vehicle;
+import org.matsim.core.controler.events.ShutdownEvent;
+import org.matsim.core.controler.events.StartupEvent;
+import org.matsim.core.controler.listener.ShutdownListener;
+import org.matsim.core.controler.listener.StartupListener;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.io.UncheckedIOException;
 
 import ch.ethz.matsim.av.electric.assets.station.Station;
+import ch.ethz.matsim.av.electric.assets.station.events.AbstractStationInteractionEvent;
 import ch.ethz.matsim.av.electric.assets.station.events.RechargeEndEvent;
 import ch.ethz.matsim.av.electric.assets.station.events.RechargeStartEvent;
 import ch.ethz.matsim.av.electric.assets.station.events.VehicleArrivalEvent;
@@ -14,35 +29,59 @@ import ch.ethz.matsim.courses.abmt17_template.RechargeEndEventHandler;
 import ch.ethz.matsim.courses.abmt17_template.RechargeStartEventHandler;
 import ch.ethz.matsim.courses.abmt17_template.VehicleArrivalEventHandler;
 
-public class StationEventHandler implements RechargeStartEventHandler, RechargeEndEventHandler, VehicleArrivalEventHandler {
+public class StationEventHandler implements GenericEventHandler {
 	
-	//final private Map<Id<Station>, Id<Vehicle>> stationUsage;
+	private final Collection<Station> stations;
+	private Map<Station,Integer> stationArrivals;
+	private Map<Station,Integer> startRecharge;
 	
-	/*public StationEventHandler(Map<Id<Station>, Id<Vehicle>> stationUsage) {
-		this.stationUsage = stationUsage;
+	public StationEventHandler(Collection<Station> stations) {
+		this.stations = stations;
+		Station[] stationArray = stations.toArray(new Station[0]);
+		this.stationArrivals = new HashMap<Station,Integer>();
+		this.startRecharge = new HashMap<Station,Integer>();
+		for (int i = 0; i < stations.size(); i++) {
+			this.stationArrivals.put(stationArray[i], 0);
+			this.startRecharge.put(stationArray[i], 0);
+		}
 	}
-	*/
+	
 	
 	@Override
-	public void handleEvent( RechargeStartEvent event ) {
+	public void handleEvent(GenericEvent event) {
+		if (event.getEventType().equals("VehicleArrival")) {
+			
+			for (Station station : this.stationArrivals.keySet()) {
+				if (station.getId().toString().equals(event.getAttributes().get("station"))) {
+					this.stationArrivals.put(station, stationArrivals.get(station) + 1);
+				}
+			}
+		}
 		
-		Id<Station> stationId = event.getStationId();
-		Id<Vehicle> vehicleId = event.getVehicleId();
-		//this.stationUsage.put(stationId, vehicleId);
-		System.out.println("Vehicle: " + vehicleId + " enters Station: " + stationId);
+		if (event.getEventType().equals("RechargeStart")) {
+			
+			for (Station station : this.startRecharge.keySet()) {
+				if (station.getId().toString().equals(event.getAttributes().get("station"))) {
+					this.startRecharge.put(station, startRecharge.get(station) + 1);
+				}
+			}
+		}
 		
+	}
+	
+	
+	
+	public Map<Station,Integer> getStationArrivals() {
+		return this.stationArrivals;
+	}
+	
+	public Map<Station,Integer> getStartRecharge() {
+		return this.startRecharge;
 	}
 
-	@Override
-	public void handleEvent(VehicleArrivalEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void handleEvent(RechargeEndEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+
+	
 
 }
